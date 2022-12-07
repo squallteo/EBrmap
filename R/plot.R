c2493e2e13cacc4f9e121950f0ac60132566a326
Hongtao Zhang <49884198+squallteo@users.noreply.github.com>
2022-12-07 10:49:31 -0500

    Second build

--- R/plot.R
+++ R/plot.R
@@ -3,39 +3,39 @@
 #' @description
 #' Plot EB-rMAP weight against various observed response in the current trial
 #'
-#' @param obj "EBrMAP" class object returned by `EB_rMAP` function
+#' @param x "EBrMAP" class object returned by `EB_rMAP` function
 #'
 #' @returns
 #' A ggplot2 object which can be further customized
 #'
 #' @export
 
-plot.EBrMAP <- function(obj){
-  if(obj$dist=="beta"){
+plot.EBrMAP <- function(x, ...){
+  if(x$dist=="beta"){
     p <-
-    obj$wdt %>%
+    x$wdt %>%
       ggplot(aes(x=y, y = w_eb)) + geom_line(size=1) +
       xlab("Observed Number of Events") + ylab("EB-rMAP Weight") + theme_bw() +
-      ggtitle(paste("Endpoint: Binomial; Sample size (N): ", obj$n, "; Gamma: ", obj$ppp_cut, sep=""))
+      ggtitle(paste("Endpoint: Binomial; Sample size (N): ", x$n, "; Gamma: ", x$ppp_cut, sep=""))
     return(p)
   }
 
-  if(obj$dist=="norm"){
+  if(x$dist=="norm"){
     p <-
-      obj$wdt %>%
+      x$wdt %>%
       ggplot(aes(x=y, y = w_eb)) + geom_line(size=1) +
-      scale_x_continuous(breaks = seq(min(obj$wdt$y), max(obj$wdt$y), length=11)) +
+      scale_x_continuous(breaks = seq(min(x$wdt$y), max(x$wdt$y), length=11)) +
       xlab("Observed Mean Response") + ylab("EB-rMAP Weight") + theme_bw() +
-      ggtitle(paste("Endpoint: Normal; Sample size (N): ", obj$n, "; Gamma: ", obj$ppp_cut, sep=""))
+      ggtitle(paste("Endpoint: Normal; Sample size (N): ", x$n, "; Gamma: ", x$ppp_cut, sep=""))
     return(p)
   }
 
-  if(obj$dist=="gamma"){
+  if(x$dist=="gamma"){
     p <-
-      obj$wdt %>%
+      x$wdt %>%
       ggplot(aes(x=y, y = w_eb)) + geom_line(size=1) +
       xlab("Observed Number of Events") + ylab("EB-rMAP Weight") + theme_bw() +
-      ggtitle(paste("Endpoint: Poisson; Sample size (Total Exposure): ", obj$n, "; Gamma: ", obj$ppp_cut, sep=""))
+      ggtitle(paste("Endpoint: Poisson; Sample size (Total Exposure): ", x$n, "; Gamma: ", x$ppp_cut, sep=""))
     return(p)
   }
 }
